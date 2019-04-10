74e444cede1f1d0c3d2b5fea3b297243ed7c795e
Stephen Webb <stephenwebb97@gmail.com>
2019-04-10 13:59:43 +1000

    Added routing and magic

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+const proxy = require('express-http-proxy');
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
@@ -11,5 +13,9 @@ app.get('/ping', function (req, res) {
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.use('/proxy', proxy('localhost:9200'));
+
+
 
 app.listen(process.env.PORT || 8080);
